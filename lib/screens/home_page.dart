ffb155c9fc3f0cafb96743e32154e03ec9ac3cdc
Alejandro Carbajal <alejandrocarbajal@MacBook-Air-de-Alejandro.local>
2022-01-05 12:22:35 -0600

    Demo de botones

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -3,6 +3,8 @@ import 'package:mining_solutions/screens/second_page.dart';
 
 import 'package:mining_solutions/widgets/button_model.dart';
 
+import '../theme.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -10,26 +12,39 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Demo de botones"),
       ),
-      body: Column(children: [
-        ButtonStyles(
-          child: Text("Confirmar Pedido"),
-          color: Colors.green,
-          border:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-        ),
-        ButtonStyles(
-          child: Text("Cancelar pedido"),
-          color: Colors.red,
-          border:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(60)),
-        ),
-        ButtonStyles(
-          child: Text("Programar envío"),
-          color: Colors.indigo,
-          border:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-        )
-      ]),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 100.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, 
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+          ButtonStyles(
+            child: Text("Confirmar Pedido", style: bodyTextStyle),
+            color: Colors.green,
+            border:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+          ),
+
+          SizedBox(height: 30.0),
+
+          ButtonStyles(
+            child: Text("Cancelar pedido", style: bodyTextStyle),
+            color: Colors.red,
+            border:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(60)),
+          ),
+
+          SizedBox(height: 30.0),
+
+
+          ButtonStyles(
+            child: Text("Programar envío", style: bodyTextStyle),
+            color: Colors.indigo,
+            border:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          )
+        ]),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_forward_ios_outlined),
         onPressed: () {
